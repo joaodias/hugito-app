df965afeec8c27c698bf0d97a4898727446d91da
Joao Dias <diasjoaoac@gmail.com>
2016-09-24 22:51:40 +0100

    Add repository validation handler
    To validate github repositories. Validation means checking if the repository has a valid HUGO file/directory tree in the master branch.
    
    Issue #18

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -20,255 +20,286 @@ var _ = Describe("Handlers", func() {
 	)
 
 	Describe("Authentication Handlers", func() {
-		var (
-			mClient    *mocks.Client
-			mux        *http.ServeMux
-			testServer *httptest.Server
-		)
 		type MockAuthentication struct {
 			Authenticated string `json:"authenticated"`
 			Code          string `json:"code"`
 			State         string `json:"state"`
 			ReceivedState string `json:"receivedState"`
 		}
+		var (
+			mClient    *mocks.Client
+			mux        *http.ServeMux
+			testServer *httptest.Server
+		)
 		BeforeEach(func() {
+			mux = http.NewServeMux()
+			testServer = httptest.NewServer(mux)
 			mClient = &mocks.Client{
 				FinishedChannels: make(map[int]chan bool),
+				OauthConf: &oauth2.Config{
+					ClientID:     "FAKE_CLIENT_ID",
+					ClientSecret: "FAKE_CLIENT_SECRET",
+					Endpoint: oauth2.Endpoint{
+						TokenURL: testServer.URL,
+					},
+				},
 			}
 		})
 		Describe("When authentication is requested", func() {
 			Context("and the JSON is invalid", func() {
-				expectedName := "error"
-				expectedData := "Error decoding json: "
 				It("should send to the client an error message", func() {
 					handlers.Authenticate(mClient, "some stuff that looks like an invalid json")
-					<-mClient.FinishedChannels[AuthenticationFinished]
-					Expect(mClient.Name).To(ContainSubstring(expectedName))
-					Expect(mClient.Data).To(ContainSubstring(expectedData))
+					Expect(mClient.Name).To(ContainSubstring("error"))
+					Expect(mClient.Data).To(ContainSubstring("Error decoding json:"))
 				})
 			})
 			Context("and the states are different", func() {
-				expectedName := "error"
-				expectedData := "received state and state are different."
 				It("should return an error", func() {
 					mockAuthentication := &MockAuthentication{Authenticated: "false", Code: "1234", State: "5678", ReceivedState: "9867"}
 					mData := structs.Map(mockAuthentication)
 					handlers.Authenticate(mClient, mData)
 					<-mClient.FinishedChannels[AuthenticationFinished]
-					Expect(mClient.Name).To(ContainSubstring(expectedName))
-					Expect(mClient.Data).To(ContainSubstring(expectedData))
+					Expect(mClient.Name).To(ContainSubstring("error"))
+					Expect(mClient.Data).To(ContainSubstring("received state and state are different."))
+				})
+			})
+			Context("and couldn't get token", func() {
+				It("should send to the client an error message", func() {
+					defer testServer.Close()
+					mockAuthentication := &MockAuthentication{Authenticated: "false", Code: "1234", State: "5678", ReceivedState: "5678"}
+					mData := structs.Map(mockAuthentication)
+					// the server is handlers is not defined so
+					// it will return a not found, which is cool.
+					handlers.Authenticate(mClient, mData)
+					<-mClient.FinishedChannels[AuthenticationFinished]
+					Expect(mClient.Name).To(ContainSubstring("error"))
+					Expect(mClient.Data).To(ContainSubstring("Error getting the access token."))
 				})
 			})
 			Context("and successfully got the token", func() {
-				expectedName := "authenticated set"
-				expectedData := "90d64460d14870c08c81352a05dedd3465940a7c"
 				It("should send to the client a authenticated set message with the token", func() {
-					mux = http.NewServeMux()
-					testServer = httptest.NewServer(mux)
+					mockAuthentication := &MockAuthentication{Authenticated: "false", Code: "1234", State: "5678", ReceivedState: "5678"}
+					mData := structs.Map(mockAuthentication)
 					defer testServer.Close()
 					mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 						fmt.Fprint(w, "access_token=90d64460d14870c08c81352a05dedd3465940a7c&scope=user&token_type=bearer")
 					})
-					mClient.OauthConf = &oauth2.Config{
-						ClientID:     "FAKE_CLIENT_ID",
-						ClientSecret: "FAKE_CLIENT_SECRET",
-						Endpoint: oauth2.Endpoint{
-							TokenURL: testServer.URL + "/token",
-						},
-					}
-					mockAuthentication := &MockAuthentication{Authenticated: "false", Code: "1234", State: "5678", ReceivedState: "5678"}
-					mData := structs.Map(mockAuthentication)
 					handlers.Authenticate(mClient, mData)
 					<-mClient.FinishedChannels[AuthenticationFinished]
-					Expect(mClient.Name).To(ContainSubstring(expectedName))
-					Expect(mClient.Data).To(ContainSubstring(expectedData))
-				})
-			})
-			Context("and couldn't get token", func() {
-				expectedName := "error"
-				expectedData := "Error getting the access token."
-				It("should send to the client an error message", func() {
-					mClient.OauthConf = &oauth2.Config{
-						ClientID:     "FAKE_CLIENT_ID",
-						ClientSecret: "FAKE_CLIENT_SECRET",
-						Endpoint: oauth2.Endpoint{
-							TokenURL: "something" + "/token",
-						},
-					}
-					mockAuthentication := &MockAuthentication{Authenticated: "false", Code: "1234", State: "5678", ReceivedState: "5678"}
-					mData := structs.Map(mockAuthentication)
-					handlers.Authenticate(mClient, mData)
-					<-mClient.FinishedChannels[AuthenticationFinished]
-					Expect(mClient.Name).To(ContainSubstring(expectedName))
-					Expect(mClient.Data).To(ContainSubstring(expectedData))
+					Expect(mClient.Name).To(ContainSubstring("authenticated set"))
+					Expect(mClient.Data).To(ContainSubstring("90d64460d14870c08c81352a05dedd3465940a7c"))
 				})
 			})
 		})
 	})
 
 	Describe("User Handlers", func() {
-		var (
-			mClient    *mocks.Client
-			mux        *http.ServeMux
-			testServer *httptest.Server
-		)
 		type MockUser struct {
 			Name        string `json:"name"`
 			AccessToken string `json:"accessToken"`
 		}
+		var (
+			mClient                *mocks.Client
+			mux                    *http.ServeMux
+			testServer             *httptest.Server
+			mockWithValidToken     *MockUser
+			mockJSONWithValidToken map[string]interface{}
+		)
 		BeforeEach(func() {
+			mux = http.NewServeMux()
+			testServer = httptest.NewServer(mux)
 			mClient = &mocks.Client{
 				FinishedChannels: make(map[int]chan bool),
+				OauthConf: &oauth2.Config{
+					ClientID:     "FAKE_CLIENT_ID",
+					ClientSecret: "FAKE_CLIENT_SECRET",
+					Endpoint: oauth2.Endpoint{
+						TokenURL: testServer.URL,
+					},
+				},
 			}
+			mockWithValidToken = &MockUser{Name: "joaodias", AccessToken: "90d64460d14870c08c81352a05dedd3465940a7c"}
+			mockJSONWithValidToken = structs.Map(mockWithValidToken)
 		})
 		Describe("When a get user is requested", func() {
 			Context("and the json is invalid", func() {
-				expectedName := "error"
-				expectedData := "Error decoding json: "
 				It("should send to the client an error message", func() {
 					handlers.GetUser(mClient, "some stuff that looks like an invalid json")
-					<-mClient.FinishedChannels[UserFinished]
-					Expect(mClient.Name).To(ContainSubstring(expectedName))
-					Expect(mClient.Data).To(ContainSubstring(expectedData))
-				})
-			})
-			Context("and the user is successfully retrieved", func() {
-				expectedName := "user set"
-				expectedData := &MockUser{Name: "joaodias", AccessToken: "90d64460d14870c08c81352a05dedd3465940a7c"}
-				It("should return a user set message to the client", func() {
-					mockWithValidToken := &MockUser{Name: "joaodias", AccessToken: "90d64460d14870c08c81352a05dedd3465940a7c"}
-					mockJsonWithValidToken := structs.Map(mockWithValidToken)
-					mux = http.NewServeMux()
-					testServer = httptest.NewServer(mux)
-					defer testServer.Close()
-					mux.HandleFunc("/user/", func(w http.ResponseWriter, r *http.Request) {
-						fmt.Fprint(w, `{"Name":"joaodias"}`)
-					})
-					mClient.OauthConf = &oauth2.Config{
-						ClientID:     "FAKE_CLIENT_ID",
-						ClientSecret: "FAKE_CLIENT_SECRET",
-						Endpoint: oauth2.Endpoint{
-							TokenURL: testServer.URL,
-						},
-					}
-					handlers.GetUser(mClient, mockJsonWithValidToken)
-					<-mClient.FinishedChannels[UserFinished]
-					receivedData := mClient.Data.(handlers.User)
-					Expect(mClient.Name).To(ContainSubstring(expectedName))
-					Expect(receivedData.Name).To(Equal(expectedData.Name))
-					Expect(receivedData.AccessToken).To(Equal(expectedData.AccessToken))
+					Expect(mClient.Name).To(ContainSubstring("error"))
+					Expect(mClient.Data).To(ContainSubstring("Error decoding json:"))
 				})
 			})
 			Context("and the user is not retrieved", func() {
-				expectedName := "logout"
-				expectedData := "Cannot get the authorized user."
+				It("should send a logout message to the client", func() {
+					defer testServer.Close()
+					mux.HandleFunc("/user/", func(w http.ResponseWriter, r *http.Request) {
+						fmt.Fprint(w, `somethingReallyWrong`)
+					})
+					handlers.GetUser(mClient, mockJSONWithValidToken)
+					<-mClient.FinishedChannels[UserFinished]
+					Expect(mClient.Name).To(ContainSubstring("logout"))
+					Expect(mClient.Data).To(Equal("Cannot get the authorized user."))
+				})
+			})
+			Context("and the user is successfully retrieved", func() {
 				It("should return a user set message to the client", func() {
-					mockWithValidToken := &MockUser{Name: "joaodias", AccessToken: "90d64460d14870c08c81352a05dedd3465940a7c"}
-					mockJsonWithValidToken := structs.Map(mockWithValidToken)
-					mux = http.NewServeMux()
-					testServer = httptest.NewServer(mux)
 					defer testServer.Close()
 					mux.HandleFunc("/user/", func(w http.ResponseWriter, r *http.Request) {
 						fmt.Fprint(w, `{"Name":"joaodias"}`)
 					})
-					// To simulate the failed test a blank url is given to the
-					// github client server. This way, the server can't get any // username and instead returns an error.
-					mClient.OauthConf = &oauth2.Config{
-						ClientID:     "FAKE_CLIENT_ID",
-						ClientSecret: "FAKE_CLIENT_SECRET",
-						Endpoint: oauth2.Endpoint{
-							TokenURL: "",
-						},
-					}
-					handlers.GetUser(mClient, mockJsonWithValidToken)
+					handlers.GetUser(mClient, mockJSONWithValidToken)
 					<-mClient.FinishedChannels[UserFinished]
-					Expect(mClient.Name).To(ContainSubstring(expectedName))
-					Expect(mClient.Data).To(Equal(expectedData))
+					receivedData := mClient.Data.(handlers.User)
+					Expect(mClient.Name).To(ContainSubstring("user set"))
+					Expect(receivedData.Name).To(Equal("joaodias"))
+					Expect(receivedData.AccessToken).To(Equal("90d64460d14870c08c81352a05dedd3465940a7c"))
 				})
 			})
 		})
 	})
 
 	Describe("Repository Handlers", func() {
-		var (
-			mClient    *mocks.Client
-			mux        *http.ServeMux
-			testServer *httptest.Server
-		)
 		type MockRepositories struct {
 			Names       []string `json:"names"`
 			AccessToken string   `json:"accessToken"`
 		}
+		type MockRepository struct {
+			Name        string `json:"name"`
+			AccessToken string `json:"accessToken"`
+		}
+		var (
+			mClient                *mocks.Client
+			mux                    *http.ServeMux
+			testServer             *httptest.Server
+			mockWithValidToken     *MockRepositories
+			mockJSONWithValidToken map[string]interface{}
+			mockValidRepo          *MockRepository
+			mockJSONValidRepo      map[string]interface{}
+			mockInvalidRepo        *MockRepository
+			mockJSONInvalidRepo    map[string]interface{}
+		)
 		BeforeEach(func() {
+			mux = http.NewServeMux()
+			testServer = httptest.NewServer(mux)
 			mClient = &mocks.Client{
 				FinishedChannels: make(map[int]chan bool),
+				OauthConf: &oauth2.Config{
+					ClientID:     "FAKE_CLIENT_ID",
+					ClientSecret: "FAKE_CLIENT_SECRET",
+					Endpoint: oauth2.Endpoint{
+						TokenURL: testServer.URL,
+					},
+				},
 			}
+			mockWithValidToken = &MockRepositories{Names: []string{""},
+				AccessToken: "90d64460d14870c08c81352a05dedd3465940a7c"}
+			mockJSONWithValidToken = structs.Map(mockWithValidToken)
+			mockValidRepo = &MockRepository{Name: "validrepo", AccessToken: "90d64460d14870c08c81352a05dedd3465940a7c"}
+			mockJSONValidRepo = structs.Map(mockValidRepo)
+			mockInvalidRepo = &MockRepository{Name: "invalidrepo", AccessToken: "90d64460d14870c08c81352a05dedd3465940a7c"}
+			mockJSONInvalidRepo = structs.Map(mockInvalidRepo)
 		})
 		Describe("When asking for user repositories", func() {
 			Context("and the JSON is invalid", func() {
-				expectedName := "error"
-				expectedData := "Error decoding json:"
 				It("should return an error to the client.", func() {
 					handlers.GetRepository(mClient, "some stuff that looks like an invalid json")
-					<-mClient.FinishedChannels[RepositoryFinished]
-					Expect(mClient.Name).To(ContainSubstring(expectedName))
-					Expect(mClient.Data).To(ContainSubstring(expectedData))
+					Expect(mClient.Name).To(ContainSubstring("error"))
+					Expect(mClient.Data).To(ContainSubstring("Error decoding json:"))
 				})
 			})
 			Context("and repositories are not retrieved", func() {
-				expectedName := "logout"
-				expectedData := "Cannot get the user repositories."
 				It("should return an error to the client.", func() {
-					mockWithValidToken := &MockRepositories{Names: []string{""}, AccessToken: "90d64460d14870c08c81352a05dedd3465940a7c"}
-					mockJsonWithValidToken := structs.Map(mockWithValidToken)
-					mux = http.NewServeMux()
-					testServer = httptest.NewServer(mux)
 					defer testServer.Close()
 					mux.HandleFunc("/user/repos/", func(w http.ResponseWriter, r *http.Request) {
-						fmt.Fprint(w, `{"Name":["repo1", "repo2", "repo3"]}`)
+						fmt.Fprint(w, `reallywrongresponse`)
 					})
-					// To simulate the failed test a blank url is given to the
-					// github client server. This way, the server can't get any // username and instead returns an error.
-					mClient.OauthConf = &oauth2.Config{
-						ClientID:     "FAKE_CLIENT_ID",
-						ClientSecret: "FAKE_CLIENT_SECRET",
-						Endpoint: oauth2.Endpoint{
-							TokenURL: "",
-						},
-					}
-					handlers.GetRepository(mClient, mockJsonWithValidToken)
+					handlers.GetRepository(mClient, mockJSONWithValidToken)
 					<-mClient.FinishedChannels[RepositoryFinished]
-					Expect(mClient.Name).To(ContainSubstring(expectedName))
-					Expect(mClient.Data).To(Equal(expectedData))
+					Expect(mClient.Name).To(ContainSubstring("logout"))
+					Expect(mClient.Data).To(Equal("Cannot get the user repositories."))
 				})
 			})
 			Context("and repositories are successfully retrieved", func() {
-				expectedName := "repository set"
-				expectedData := &MockRepositories{Names: []string{"repo1", "repo2", "repo3"}, AccessToken: "90d64460d14870c08c81352a05dedd3465940a7c"}
 				It("should return the retrieved repositories to the client.", func() {
-					mockWithValidToken := &MockRepositories{Names: []string{""}, AccessToken: "90d64460d14870c08c81352a05dedd3465940a7c"}
-					mockJsonWithValidToken := structs.Map(mockWithValidToken)
-					mux = http.NewServeMux()
-					testServer = httptest.NewServer(mux)
-					defer testServer.Close()
 					mux.HandleFunc("/user/repos/", func(w http.ResponseWriter, r *http.Request) {
 						fmt.Fprint(w, `[{"Name":"repo1"}, {"Name":"repo2"}, {"Name":"repo3"}]`)
 					})
-					// To simulate the failed test a blank url is given to the
-					// github client server. This way, the server can't get any // username and instead returns an error.
-					mClient.OauthConf = &oauth2.Config{
-						ClientID:     "FAKE_CLIENT_ID",
-						ClientSecret: "FAKE_CLIENT_SECRET",
-						Endpoint: oauth2.Endpoint{
-							TokenURL: testServer.URL,
-						},
-					}
-					handlers.GetRepository(mClient, mockJsonWithValidToken)
+					handlers.GetRepository(mClient, mockJSONWithValidToken)
 					<-mClient.FinishedChannels[RepositoryFinished]
 					receivedData := mClient.Data.(handlers.Repositories)
-					Expect(mClient.Name).To(ContainSubstring(expectedName))
-					Expect(receivedData.Names).To(Equal(expectedData.Names))
-					Expect(receivedData.AccessToken).To(Equal(expectedData.AccessToken))
+					Expect(mClient.Name).To(ContainSubstring("repository set"))
+					Expect(receivedData.Names).To(Equal([]string{"repo1", "repo2", "repo3"}))
+					Expect(receivedData.AccessToken).To(Equal("90d64460d14870c08c81352a05dedd3465940a7c"))
+				})
+			})
+		})
+
+		Describe("When checking the validity of a repository", func() {
+			Context("and the JSON is invalid", func() {
+				It("should return an error to the client", func() {
+					handlers.ValidateRepository(mClient, "some stuff that looks like an invalid json")
+					Expect(mClient.Name).To(ContainSubstring("error"))
+					Expect(mClient.Data).To(ContainSubstring("Error decoding json:"))
+				})
+			})
+			Context("and the user Login cannot be retrieved", func() {
+				It("should return a  logout message to the client", func() {
+					defer testServer.Close()
+					mux.HandleFunc("/user/", func(w http.ResponseWriter, r *http.Request) {
+						fmt.Fprint(w, `someErronicThingHappened`)
+					})
+					mux.HandleFunc("/repos/joaodias/validrepo/contents/", func(w http.ResponseWriter, r *http.Request) {
+						fmt.Fprint(w, `[{"Name":"content"}, {"Name":"config.toml"}, {"Name":"public"}, {"Name":"themes"}]`)
+					})
+					handlers.ValidateRepository(mClient, mockJSONValidRepo)
+					<-mClient.FinishedChannels[RepositoryFinished]
+					Expect(mClient.Name).To(Equal("logout"))
+					Expect(mClient.Data).To(Equal("Can't retrieve the authenticated user."))
+				})
+			})
+			Context("and the repository tree cannot be retrieved", func() {
+				It("should return an error message to the client", func() {
+					defer testServer.Close()
+					mux.HandleFunc("/user/", func(w http.ResponseWriter, r *http.Request) {
+						fmt.Fprint(w, `{"Login":"joaodias"}`)
+					})
+					mux.HandleFunc("/repos/joaodias/validrepo/contents/", func(w http.ResponseWriter, r *http.Request) {
+						fmt.Fprint(w, `someErroniousStuff`)
+					})
+					handlers.ValidateRepository(mClient, mockJSONValidRepo)
+					<-mClient.FinishedChannels[RepositoryFinished]
+					Expect(mClient.Name).To(Equal("error"))
+					Expect(mClient.Data).To(Equal("Can't retrieve selected repository."))
+				})
+			})
+			Context("Repository is invalid", func() {
+				It("should return error with repository not valid to the client", func() {
+					defer testServer.Close()
+					mux.HandleFunc("/user/", func(w http.ResponseWriter, r *http.Request) {
+						fmt.Fprint(w, `{"Login":"joaodias"}`)
+					})
+					mux.HandleFunc("/repos/joaodias/invalidrepo/contents/", func(w http.ResponseWriter, r *http.Request) {
+						fmt.Fprint(w, `[{"Name":"dir1"}, {"Name":"file1"}, {"Name":"dir2"}]`)
+					})
+					handlers.ValidateRepository(mClient, mockJSONInvalidRepo)
+					<-mClient.FinishedChannels[RepositoryFinished]
+					Expect(mClient.Name).To(Equal("error"))
+					Expect(mClient.Data).To(Equal("Repository is not valid."))
+				})
+			})
+			Context("Repository is valid", func() {
+				It("should return true to the client", func() {
+					defer testServer.Close()
+					mux.HandleFunc("/user/", func(w http.ResponseWriter, r *http.Request) {
+						fmt.Fprint(w, `{"Login":"joaodias"}`)
+					})
+					mux.HandleFunc("/repos/joaodias/validrepo/contents/", func(w http.ResponseWriter, r *http.Request) {
+						fmt.Fprint(w, `[{"Name":"content"}, {"Name":"config.toml"}, {"Name":"public"}, {"Name":"themes"}]`)
+					})
+					handlers.ValidateRepository(mClient, mockJSONValidRepo)
+					<-mClient.FinishedChannels[RepositoryFinished]
+					Expect(mClient.Name).To(Equal("repository validate"))
+					Expect(mClient.Data).To(Equal("Repository is valid."))
 				})
 			})
 		})
--- handlers/repository.go
+++ handlers/repository.go
@@ -18,15 +18,14 @@ type Repositories struct {
 // 3. Get the repositories for the user
 // 4. Send repositories to the client
 func GetRepository(communicator Communicator, data interface{}) {
-	communicator.NewFinishedChannel(RepositoryFinished)
 	var repositories Repositories
+	err := mapstructure.Decode(data, &repositories)
+	if err != nil {
+		communicator.SetSend("error", "Error decoding json:"+err.Error())
+		return
+	}
+	communicator.NewFinishedChannel(RepositoryFinished)
 	go func() {
-		err := mapstructure.Decode(data, &repositories)
-		if err != nil {
-			communicator.SetSend("error", "Error decoding json:"+err.Error())
-			communicator.Finished(RepositoryFinished)
-			return
-		}
 		githubClient := GetGithubClient(repositories.AccessToken, communicator)
 		repositories.Names, err = GetGithubRepositories(githubClient)
 		if err != nil {
@@ -51,6 +50,48 @@ func RemoveRepository(communicator Communicator, data interface{}) {
 	fmt.Print("Remove repository \n")
 }
 
+// ValidateRepository checks if a given repository has a valid hugo
+// configuration in the master branch.
+//
+// Happy Path:
+// 1. Decode JSON
+// 2. Get the github authenticated user
+// 3. Get the repository file structure from github
+// 4. Check if the given file structure contains the reference file structure.
+// 5. Send a repository validate message to the client
 func ValidateRepository(communicator Communicator, data interface{}) {
-	fmt.Print("Validate repository \n")
+	type Repository struct {
+		Name        string `json:"name"`
+		AccessToken string `json:"accessToken"`
+	}
+	var repository Repository
+	err := mapstructure.Decode(data, &repository)
+	if err != nil {
+		communicator.SetSend("error", "Error decoding json:"+err.Error())
+		return
+	}
+	communicator.NewFinishedChannel(RepositoryFinished)
+	go func() {
+		githubClient := GetGithubClient(repository.AccessToken, communicator)
+		userLogin, err := GetGithubUserLogin(githubClient)
+		if err != nil {
+			communicator.SetSend("logout", "Can't retrieve the authenticated user.")
+			communicator.Finished(RepositoryFinished)
+			return
+		}
+		repositoryTree, err := GetGithubRepositoryTree(githubClient, userLogin, repository.Name)
+		if err != nil {
+			communicator.SetSend("error", "Can't retrieve selected repository.")
+			communicator.Finished(RepositoryFinished)
+			return
+		}
+		isValid := IsGithubRepositoryValid(repositoryTree)
+		if !isValid {
+			communicator.SetSend("error", "Repository is not valid.")
+			communicator.Finished(RepositoryFinished)
+			return
+		}
+		communicator.SetSend("repository validate", "Repository is valid.")
+		communicator.Finished(RepositoryFinished)
+	}()
 }
