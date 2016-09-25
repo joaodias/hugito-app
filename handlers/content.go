99929148dc55fa81baf0cf5e1b69181cfda742f2
Joao Dias <diasjoaoac@gmail.com>
2016-09-25 17:07:11 +0100

    Add get content list functionality
    Issue #18

--- handlers/content.go
+++ handlers/content.go
@@ -1,32 +1,53 @@
 package handlers
 
 import (
-    "fmt"
+	"github.com/mitchellh/mapstructure"
 )
+
+// ContentList represents the list of content for a given repository name
+type ContentList struct {
+	Name        string   `json:"name"`
+	Titles      []string `json:"title"`
+	AccessToken string   `json:"accessToken"`
+}
 
 // Content represents the information exchanged between the server and the client.
 type Content struct {
-    title  string
-    author string
-    date   string
+	title  string
+	author string
+	date   string
 }
 
-func SubscribeContent(communicator Communicator, data interface{}) {
-    fmt.Print("Subscribe content \n")
-}
-
-func UnsubscribeContent(communicator Communicator, data interface{}) {
-    fmt.Print("Unsubscribe content \n")
-}
-
-func AddContent(communicator Communicator, data interface{}) {
-    fmt.Print("Add content \n")
-}
-
-func RemoveContent(communicator Communicator, data interface{}) {
-    fmt.Print("Remove content \n")
-}
-
-func UpdateContent(communicator Communicator, data interface{}) {
-    fmt.Print("Update content \n")
+// GetContentList gets the repository content list.
+//
+// Happy Path:
+// 1. Decode JSON
+// 2. Get the github authenticated user
+// 3. Get the repository content files
+// 4. Send the content list to the client
+func GetContentList(communicator Communicator, data interface{}) {
+	var contentList ContentList
+	err := mapstructure.Decode(data, &contentList)
+	if err != nil {
+		communicator.SetSend("error", "Error decoding json:"+err.Error())
+		return
+	}
+	communicator.NewFinishedChannel(ContentFinished)
+	go func() {
+		githubClient := GetGithubClient(contentList.AccessToken, communicator)
+		userLogin, err := GetGithubUserLogin(githubClient)
+		if err != nil {
+			communicator.SetSend("logout", "Can't retrieve the authenticated user.")
+			communicator.Finished(ContentFinished)
+			return
+		}
+		contentList.Titles, err = GetGithubRepositoryTree(githubClient, userLogin, contentList.Name, "content")
+		if err != nil {
+			communicator.SetSend("error", "Can't retrieve the content list.")
+			communicator.Finished(ContentFinished)
+			return
+		}
+		communicator.SetSend("content list", contentList)
+		communicator.Finished(ContentFinished)
+	}()
 }
