1afa138f518a132097eb7a180a09e6179de860a5
Joao Dias <diasjoaoac@gmail.com>
2016-10-06 00:58:06 +0100

    Refactor router messages
    To support the correct client calls.
    
    Issue #26

--- main.go
+++ main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-	"fmt"
+	log "github.com/Sirupsen/logrus"
 	handlers "github.com/joaodias/hugito-app/handlers"
 	"net/http"
 )
 
 func main() {
 	router := handlers.NewRouter()
-	router.Handle("repository get", handlers.GetRepository)
+	router.Handle("repositories get", handlers.GetRepository)
 	router.Handle("repository validate", handlers.ValidateRepository)
 	router.Handle("content list", handlers.GetContentList)
+	router.Handle("content get", handlers.GetFileContent)
 	router.Handle("user get", handlers.GetUser)
 	router.Handle("authenticate", handlers.Authenticate)
 	http.Handle("/", router)
-	fmt.Print("Go app initialized on port 4000 \n")
+	log.Info("Go app initialized in port 4000.")
 	http.ListenAndServe(":4000", nil)
 }
